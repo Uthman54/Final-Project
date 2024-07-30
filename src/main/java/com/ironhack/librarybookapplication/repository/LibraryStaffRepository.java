cfa93250f508d2304b52c2241cff97182fbc4467
uthman54 <uthmanbh54@gmail.com>
2024-07-30 19:44:15 +0200

    Final Project

--- src/main/java/com/ironhack/librarybookapplication/repository/LibraryStaffRepository.java
+++ src/main/java/com/ironhack/librarybookapplication/repository/LibraryStaffRepository.java
@@ -4,8 +4,6 @@ import com.ironhack.librarybookapplication.model.LibraryStaff;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface LibraryStaffRepository extends JpaRepository<LibraryStaff, Integer> {
